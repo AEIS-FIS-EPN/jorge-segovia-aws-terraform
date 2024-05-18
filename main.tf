ed31173729e019f1d829a27e64a4ab350d9b37fc
Jorge Segovia <148887035+JorgeSegovia98@users.noreply.github.com>
2024-05-18 09:21:54 -0500

    chore: Add a security group to allow traffic

--- main.tf
+++ main.tf
@@ -41,3 +41,59 @@ resource "aws_route_table_association" "fis_public_association" {
   route_table_id = aws_route_table.fis_public_subnet_route_table.id
   subnet_id = aws_subnet.public_subnet.id
 }
+
+resource "aws_security_group" "web_server_sg" {
+  vpc_id = aws_vpc.fis_vpc.id
+
+  ingress{
+    description = "Allow HTTP traffic form internet"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress{
+    description = "Allow HTTPS traffic form internet"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all trafic"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "aeis security group"
+  }
+
+}
+
+//Data
+data "aws_ami" "ubuntu" {
+  most_recent = "true"
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = "099720109477"
+}
+
+resource "aws_instance" "ubuntu_aeis_instance" {
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t2.macro"
+  subnet_id = aws_subnet.public_subnet.id
+}
